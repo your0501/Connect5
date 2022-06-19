1e3c54b60edbd007ccdc342cfa50214d22e3142f
your0501 <57907299+your0501@users.noreply.github.com>
2022-06-19 22:04:21 +0900

    클래스화, 기존 코드 모두 제거

--- main.js
+++ main.js
@@ -1,38 +1,146 @@
 
 const App = new PIXI.Application()
 document.body.appendChild(App.view)
+App.view.addEventListener('contextmenu', (e) => { e.preventDefault(); });
+
 
 class RenjuBoard extends PIXI.Sprite {
+    noneFunction = function() {}
+
     constructor() {
-        let texture = PIXI.Texture.from("./assets/renjuBoard.png");
+        let texture = PIXI.Texture.from("./images/renjuBoard.png");
         super(texture);
+		this.interactive = true;
+		this.on("pointerdown", this.onClicked);
+		this.on("pointermove", this.onMove);
         this.initalize();
     }
+	
     initalize() {
         this.map = new Array(15).fill(0).map(a => new Uint8Array(15)); // 15x15 사이즈 배열
+		this.pieces = [];
+		this.piecesIndex = 0; // 지금 넣을 요소의 인덱스
+        this.phase = 'none'
+        this.LeftCallback = this.noneFunction;
 
+        this.cursor = new PIXI.Sprite.from("./images/cursor.png");
+        this.addChild(this.cursor);
     }
+	
+    getMap() {
+        return this.map;
+    }
+
+    getCell(x, y) {
+        if (0 <= x && x < 15 && 0 <= y && y < 15) {
+            return this.map[y][x]
+        }
+        return undefined;
+    }
+
+    setPhase(phase) {
+        this.phase = phase;
+    }
+
+    getPhase() {
+        return this.phase;
+    }
+
+    setCallback(func) {
+        this.LeftCallback = func;
+    }
+
+	setPiece(color, x, y) {
+		let piece = new RenjuPiece(color, x, y);
+
+        //while (this.pieces.length > this.piecesIndex) {
+        //    this.removeChild(this.pieces.pop()); // 수를 물러서 뒤로 간 만큼 지운다
+        //}
+
+        this.pieces[this.piecesIndex] = piece;
+        this.piecesIndex++;
+        this.map[y][x] = color;
+        
+		this.addChild(piece);
+	}
+	
+	onClicked = ie => {
+		switch(ie.data.button) {
+			case 0:
+				this.onLeftClick(ie);
+				break;
+			case 1:
+				//this.redo(ie);
+				break;
+			case 2:
+				//this.undo(ie);
+				break;
+				
+		}
+		
+
+	}
+	
+    onLeftClick(interactionEvent) {
+        if (this.phase == 'black' || this.phase == 'white') {
+            const pos = interactionEvent.data.getLocalPosition(this);
+            const x = Math.max(Math.min(Math.floor(pos.x / 32), 14), 0);
+            const y = Math.max(Math.min(Math.floor(pos.y / 32), 14), 0);
+            this.LeftCallback(x, y);
+        }
+    }
+    
+    onMove(interactionEvent) {
+        if (this.phase == 'black' || this.phase == 'white') {
+            const pos = interactionEvent.data.getLocalPosition(this);
+            const x = Math.max(Math.min(Math.floor(pos.x / 32), 14), 0);
+            const y = Math.max(Math.min(Math.floor(pos.y / 32), 14), 0);
+            this.cursor.x = x * 32
+            this.cursor.y = y * 32
+        }
+    }
+
+	dropPiece(ie) { // no use
+        if (this.phase == 'black' || this.phase == 'white') {
+            const pos = ie.data.getLocalPosition(this);
+            const x = Math.max(Math.min(Math.floor(pos.x / 32), 14), 0);
+            const y = Math.max(Math.min(Math.floor(pos.y / 32), 14), 0);
+            const color = (this.phase == 'black') ? RenjuManager.BLACK : RenjuManager.WHITE;
+            this.setPiece(color, x, y);
+            this.dropCallback(ie);
+        }
+
+	}
+	
+	undo(ie) {
+		if (this.piecesIndex - 1 >= 0) {
+			this.pieces[this.piecesIndex - 1].visible = false;
+			this.piecesIndex--;
+		}
+	}
+	
+	redo(ie) {
+		if (this.pieces.length > this.piecesIndex) {
+			this.pieces[this.piecesIndex].visible = true;
+			this.piecesIndex++;
+		}
+	}
 }
 
 class RenjuPiece extends PIXI.Sprite {
-    pieceTextures = [PIXI.Texture.from("./assets/renjuBlack.png"), PIXI.Texture.from("./assets/renjuWhite.png")]
+    static pieceTextures = [0, PIXI.Texture.from("./images/renjuBlack.png"), PIXI.Texture.from("./images/renjuWhite.png")]
+	
     constructor(color, x, y) {
         super();
         this.initalize(color, x, y);
     }
+	
     initalize(color, x, y) {
-        this.texture = this.pieceTextures?.[color] ?? this.pieceTextures[0];
+        this.texture = RenjuPiece.pieceTextures?.[color] ?? this.pieceTextures[0];
         this.x = x * 32;
         this.y = y * 32;
     }
-    s() {
-        this.rotation += Math.random() / 2;
-        this.x += Math.random();
-        this.y += Math.random();
-        this.x -= Math.random();
-        this.y -= Math.random();
 
-    }
 }
 
 class RenjuPlayer {
@@ -42,196 +150,123 @@ class RenjuPlayer {
     }
 }
 
-class Input {
-    constructor() {
-        this.x = 0;
-        this.y = 0;
-        this.cursor = new PIXI.Sprite.from("./assets/cursor.png");
-        App.stage.addChild(this.cursor);
-    }
-
-    _mouse_move = (e) => {
-        let x = Math.max(Math.min(Math.floor(e.offsetX / 32), 14), 0);
-        let y = Math.max(Math.min(Math.floor(e.offsetY / 32), 14), 0);
-        this.cursor.x = x * 32;
-        this.cursor.y = y * 32;
-    }
-
-}
 
 class RenjuManager {
+	static BLACK = 1;
+	static WHITE = 2;
+	
     constructor() {
-        this.turn = 'black'
+        this.phase = RenjuManager.BLACK;
+
+        this.audioManger = new AudioManager();
+
+        this.renjuBoard = new RenjuBoard();
+        this.renjuBoard.setPhase('black')
+        this.renjuBoard.setCallback(this.onBoardleftClick.bind(this))
+
+        App.stage.addChild(this.renjuBoard);
     }
-}
 
-const renjuBoard = new RenjuBoard();
-App.stage.addChild(renjuBoard)
-const input = new Input();
-document.addEventListener('mousemove', input._mouse_move)
- 
-const map = new Array(15).fill(0).map(() => new Array(15).fill(0));
+    onBoardleftClick(x, y) {
+        this.audioManger.playBgm(); // 하나 둔 이후에야 BGM 시작
+        this.drop(x, y);
+    }
 
+    drop(x, y) {
+        if (this.canDrop(x, y)) {
+            this.renjuBoard.setPiece(this.phase, x, y);
+            this.audioManger.playHit();
+            this.checkWin();
+            this.changeTurn();
+        }
+    }
 
-let color = 0;
-let turn = 0;
+    checkWin() {
+        const win = this.find5();
+        if (win == RenjuManager.BLACK || win == RenjuManager.WHITE) {
+            alert(`${win == RenjuManager.BLACK? '흑': '백'}승`);
+            this.phase = 'none';
+            this.renjuBoard.setPhase('none');
+        }
 
+    }
 
-function step(f=false, ft=0) {
-    const iss = ([].concat(...map)).reduce((acc, cur, i) => { if (cur === 0) acc.push(i); return acc; }, []);
+    canDrop(x, y) {
+        return this.isEmpty(x, y) && !this.isRestricted(x, y);
+    }
+
+    isEmpty(x, y) {
+        return this.renjuBoard.getCell(x, y) == 0;
+    }
+
+    isRestricted(x, y) {
+        if (this.phase == RenjuManager.WHITE) return false;
+        return this.is33(x, y);
+    }
+
+    is33(x, y) {
+        return false;
+    }
     
-    const r = iss[Math.floor(Math.random() * iss.length)];
-    let x = r % 15;
-    let y = Math.floor(r / 15);
-
-    if (turn === 0) {
-        x = 7
-        y = 7
-    }
-
-
-    map[y][x] = 1 + color;
-    ctx.fillStyle = color === 0 ? '#000' : '#FFF';
-    console.log(iss, r, x, y);
-    ctx.beginPath();
-    ctx.arc(x * 32 + 16, y * 32 + 16, 15, 0, 2 * Math.PI);
-    ctx.fill();
-    ctx.closePath();
-
-    color = 1 - color;
-
-    if (f === true) {
-        color = ft
-    }
-    let c = connect5();
-    switch(c) {
-        case 1:
-            ctx.fillStyle = '#000000';
-            ctx.font = 'bold 30px Arial';
-            ctx.fillText('흑 승리', canvas.width / 2 - 50, canvas.height / 2);
-            break;
-        case 2:
-            ctx.fillStyle = '#FFFFFF';
-            ctx.font = 'bold 30px Arial';
-            ctx.fillText('백 승리', canvas.width / 2 - 50, canvas.height / 2);
-            break;
-    }
-
-    turn++;
-} 
-
-
-function check_win() {
-    let c = connect5();
-    switch(c) {
-        case 1:
-            ctx.fillStyle = '#000000';
-            ctx.font = 'bold 30px Arial';
-            ctx.fillText('흑 승리', canvas.width / 2 - 50, canvas.height / 2);
-            break;
-        case 2:
-            ctx.fillStyle = '#FFFFFF';
-            ctx.font = 'bold 30px Arial';
-            ctx.fillText('백 승리', canvas.width / 2 - 50, canvas.height / 2);
-            break;
-    }
-}
-
-function drop(x, y, color) {
-    map[y][x] = 1 + color;
-
-    ctx.fillStyle = color === 0 ? '#000' : '#FFF';
-
-    ctx.beginPath();
-    ctx.arc(x * 32 + 16, y * 32 + 16, 15, 0, 2 * Math.PI);
-    ctx.fill();
-    ctx.closePath();
-
-    color = 1 - color;
-
-    turn++;
-} 
-
-
-function click(e) {
-    const x = e.offsetX;
-    const y = e.offsetY;
-    const i = Math.floor((x) / 32);
-    const j = Math.floor((y) / 32);
-    if (i < 0 || i > 14 || j < 0 || j > 14) return;
-    if (map[j][i] !== 0) return;
-    const color = turn % 2;
-    if (samsamRestrict(i, j)) { 
-        console.log("삼삼")
-    } else {
-        drop(i, j, color);
-        check_win();
-    }
-
-
-}
-
-
-// map 배열에서 같은 숫자가 가로, 세로, 대각선으로 5개 나열되어있는지 확인하는 함수
-function connect5() {
-    for (let i = 0; i < 15; i++) {
-        for (let j = 0; j < 15; j++) {
-            if (!map[i]?.[j]) continue;
-            if (map[i]?.[j] === map[i]?.[j + 1] && map[i]?.[j] === map[i]?.[j + 2] && map[i]?.[j] === map[i]?.[j + 3] && map[i]?.[j] === map[i]?.[j + 4]) {
-                return map[i]?.[j];
-            }
-            if (map[i]?.[j] === map[i + 1]?.[j] && map[i]?.[j] === map[i + 2]?.[j] && map[i]?.[j] === map[i + 3]?.[j] && map[i]?.[j] === map[i + 4]?.[j]) {
-                return map[i]?.[j];
-            }
-            if (map[i]?.[j] === map[i + 1]?.[j + 1] && map[i]?.[j] === map[i + 2]?.[j + 2] && map[i]?.[j] === map[i + 3]?.[j + 3] && map[i]?.[j] === map[i + 4]?.[j + 4]) {
-                return map[i]?.[j];
-            }
-            if (map[i]?.[j] === map[i - 1]?.[j + 1] && map[i]?.[j] === map[i - 2]?.[j + 2] && map[i]?.[j] === map[i - 3]?.[j + 3] && map[i]?.[j] === map[i - 4]?.[j + 4]) {
-                return map[i]?.[j];
-            }
-        }
-    }
-    return 0;
-}
-
-// map에서 열린삼을 확인한다.
-const openSamList = ["01110", "011010", "010110"];
-function findOpenSam(color) {
-    let samCount = 0;
-    for (let i = 0; i < 15; i++) {
-        for (let j = 0; j < 15; j++) {
-
-            for (let sam of openSamList) { // 패턴으로 열린삼 찾기
-                let pattern = sam.split("").map(a => parseInt(a));
-
-                for (let dir of [[1, 0], [0, 1], [1, 1], [-1, 1]]) {
-                    let valid = pattern.reduce((prevReturn, currValue, index) => {
-                        if (currValue == map[i + index * dir[0]]?.[j + index * dir[1]] && prevReturn) {
-                            return true;
-                        }
-                        return false;
-                    }, true);
-                    if (valid) {
-                        samCount += 1;
-                    };
+    find5() {
+        const map = this.renjuBoard.getCell.bind(this.renjuBoard);
+        for (let y = 0; y < 15; y++) {
+            for (let x = 0; x < 15; x++) {
+                if (!map(x, y)) continue;
+                if (this.checkAllEqual(map(x, y), map(x, y+1), map(x, y+2), map(x, y+3), map(x, y+4)) || // 가로
+                     this.checkAllEqual(map(x, y), map(x+1, y), map(x+2, y), map(x+3, y), map(x+4, y)) || // 세로
+                      this.checkAllEqual(map(x, y), map(x+1, y+1), map(x+2, y+2), map(x+3, y+3), map(x+4, y+4)) || // ↘대각선
+                       this.checkAllEqual(map(x, y), map(x-1, y+1), map(x-2, y+2), map(x-3, y+3), map(x-4, y+4))) { // ↙대각선
+                    return map(x, y);
                 }
-
             }
-            
+        }
+        return 0;
+    }
+
+    changeTurn() {
+        if (this.phase == RenjuManager.BLACK) {
+            this.phase = RenjuManager.WHITE;
+            this.renjuBoard.setPhase('white')
+        } else if (this.phase == RenjuManager.WHITE) {
+            this.phase = RenjuManager.BLACK;
+            this.renjuBoard.setPhase('black')
         }
     }
-    console.log("흑의 열린 3 갯수: ", samCount)
-    return samCount;
+
+    checkAllEqual() {
+        const arr = Array.from(arguments);
+        if (arr.length < 2) return true;
+        const base = arr[0];
+        return arr.reduce((p, c) => p && (c == base), true);
+    }
 }
 
-function samsamRestrict(x, y) {
-    const currSamCount = findOpenSam();
-    if (currSamCount > 0) return false;
-    map[x][y] = 1;
-    const nextSamCount = findOpenSam();
-    map[x][y] = 0;
-    if (nextSamCount > currSamCount + 1) {
-        return true
-    };
-    return false;
+
+class AudioManager {
+    constructor() {
+        this.bgm = new Audio('./audios/n57.mp3')
+        this.bgm.addEventListener('ended', function() {
+            this.currentTile = 0;
+            this.play();
+        });
+        this.hit = new Audio('./audios/placement.mp3');
+        this.bgmPlayed = false;
+    }
+
+    playBgm() {
+        if (this.bgmPlayed) return;
+        this.bgm.play();
+        this.bgmPlayed = true;
+    }
+
+    playHit() {
+        this.hit.play();
+    }
 }
+
+
+
+const renjuManager = new RenjuManager();
+
+
